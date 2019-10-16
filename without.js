55a9370c82a828710c59bfaead47a6f485de907f
LAYTHJABBAR <layth.jabbar@yahoo.com>
2019-10-16 07:16:14 +0000

    withoutjs

---
+++ without.js
@@ -0,0 +1,42 @@
+const without = function(source, itemsToRemove) {
+  let output = source;
+  itemsToRemove.forEach(toRemove => {
+    output = output.filter(elem => elem !== toRemove);
+  });
+  console.log(output)
+  return output;
+};
+const eqArrays = function(a , b ) {
+  if (a.length !== b.length){
+    return false
+  } 
+  else {
+    for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+  }}
+return true
+}}
+const 
+assertArraysEqual = function(actual, expected) {
+  const match = eqArrays(actual,expected)
+  if ( match) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  else {
+    console.log(`🛑🛑🛑Asseration Passed: ${actual} !== ${expected}`);
+  }
+  return actual;
+};
+
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+
+
+
+
+
+

f055f586b44c8dc6c757a604db0a6a45541d5095
LAYTHJABBAR <layth.jabbar@yahoo.com>
2019-10-16 22:48:50 +0000

    counterletter

---
+++ countLetters.js
@@ -0,0 +1,36 @@
+
+const countLetters = function(string) {
+  const noSpace = string.split(' ').join('');
+  console.log(noSpace)
+  const letterCounter = {};
+
+  for (const letter of noSpace) {
+    if (!letterCounter[letter]) {
+    letterCounter[letter] = 1;
+}
+  else {
+       letterCounter[letter] +=1
+      }
+  }console.log(letterCounter)
+  return letterCounter;
+};
+
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  else {
+    console.log(`🛑🛑🛑Asseration Passed: ${actual} !== ${expected}`);
+  }
+  return actual;
+};
+
+const string = countLetters("laa yth");
+assertEqual(string["l"],1 );
+assertEqual(string["a"],1 );
+assertEqual(string["y"],1 );
+assertEqual(string["t"],1 );
+assertEqual(string["h"],1 );

91a7790eb111fe2c9f30cb2126fd7942d449cf59
LAYTHJABBAR <layth.jabbar@yahoo.com>
2019-10-16 22:14:32 +0000

    countOnly

---
+++ countOnly.js
@@ -0,0 +1,45 @@
+const countOnly = function(item, itemsToCount) {
+  var result = {};
+  for (i =0 ; i< firstNames.length; i++){
+    if (itemsToCount[firstNames[i]]) {
+        
+        if (!result[firstNames[i]])
+      {
+        result[firstNames[i]] = 1
+      } else {
+       result[firstNames[i]] +=1
+      }
+    }
+  }
+      console.log(result)
+    return result;    
+} 
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  else {
+    console.log(`🛑🛑🛑Asseration Passed: ${actual} !== ${expected}`);
+  }
+  return actual;
+};
+ 
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+const conditions = { "Jason": true, "Karima": true, "Fang": true, "Kavith": false }
+const result1 = countOnly(firstNames, conditions);
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+
+

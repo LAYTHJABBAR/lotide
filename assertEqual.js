12c50e55ff2dbb2a3e864f06a9c8ca7813940158
LAYTHJABBAR <layth.jabbar@yahoo.com>
2019-10-17 01:34:08 +0000

    a

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+
+=======
+>>>>>>> 19ec378a7b51b5a239cc9c9a6e1ae72a3cc5cead
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  else {
+    console.log(`🛑🛑🛑Asseration Failed: ${actual} !== ${expected}`);
+  }
+  return actual;
+};
+<<<<<<< HEAD
+
+
+=======
+>>>>>>> 19ec378a7b51b5a239cc9c9a6e1ae72a3cc5cead

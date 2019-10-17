19ec378a7b51b5a239cc9c9a6e1ae72a3cc5cead
LAYTHJABBAR <53024934+LAYTHJABBAR@users.noreply.github.com>
2019-10-16 19:02:42 -0600

    Update assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,23 +1,3 @@
-
-const countLetters = function(string) {
-  const noSpace = string.split(' ').join('');
-  console.log(noSpace)
-  const letterCounter = {};
-
-  for (const letter of noSpace) {
-    if (!letterCounter[letter]) {
-    letterCounter[letter] = 1;
-}
-    else {
-       letterCounter[letter] +=1
-      }
-  }console.log(letterCounter)
-  return letterCounter;
-};
-
-
-
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -27,10 +7,3 @@ const assertEqual = function(actual, expected) {
   }
   return actual;
 };
-
-const string = countLetters("laa yth");
-assertEqual(string["l"],1 );
-assertEqual(string["a"],1 );
-assertEqual(string["y"],1 );
-assertEqual(string["t"],1 );
-assertEqual(string["h"],1 );
